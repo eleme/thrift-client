2311138fab42868964cebf247c1a6148437faeed
YanagiEiichi <admin@web-tinker.com>
2016-07-12 14:21:30 +0800

    Use EventEmitter

--- thrift-client.js
+++ thrift-client.js
@@ -1,53 +1,97 @@
 const Thrift = require('node-thrift-protocol');
 const Storage = require('./lib/storage');
 const ThriftSchema = require('./lib/thrift-schema');
+const { EventEmitter } = require('events');
 
 const METHODS = Symbol();
 const STORAGE = Symbol();
-const RECEIVE = Symbol();
-const ERROR = Symbol();
 
 class ThriftListener {
-
-  /**
-   * Public
-   **/
   constructor({ port, schema }) {
-    let pool = new Set();
+    Object.defineProperty(this, METHODS, { value: [] });
     Thrift.createServer(thrift => {
       let client = new ThriftClient({ thrift, schema });
       this[METHODS].forEach(args => client.register(...args));
-      pool.add(client);
-      thrift.on('close', () => pool.delete(client));
     }).listen(port);
   }
   register(...args) {
     this[METHODS].push(args);
     return this;
   }
-
-  /**
-   * Private
-   **/
-  get [METHODS]() {
-    let value = [];
-    Object.defineProperty(this, METHODS, { value });
-    return value;
-  }
-
 }
 
-class ThriftClient {
 
-  /**
-   * Public
-   **/
+/**
+ * Process a connection error
+**/
+function tcError(that, reason) {
+  that[STORAGE].takeForEach(({ reject }) => reject(reason));
+  if (that.retryDefer > 0) setTimeout(() => that.resetThrift(), that.retryDefer);
+  that.emit('error', reason);
+}
+
+
+/**
+ * Process a thrift frame
+**/
+function tcReceive(that, { id, type, name, fields }) {
+  let api = that.schema.service[name];
+  switch (type) {
+    case 'CALL':
+      let params = that.schema.decodeStruct(api.args, { fields });
+      that.trigger(name, params).then(result => {
+        result = that.schema.encodeValueWithType(result, api.type);
+        result.id = 0;
+        let fields = [ result ];
+        that.thrift.write({ id, type: 'REPLY', name, fields });
+      }, error => {
+        let fields;
+        try {
+          fields = that.schema.encodeStruct(api.throws, error).fields;
+          if (!fields.length) throw error;
+        } catch (error) {
+          let { name, message } = error;
+          fields = [ { id: 999, type: 'STRING', value: JSON.stringify({ name, message }) } ];
+        }
+        that.thrift.write({ id, type: 'REPLY', name, fields });
+      });
+      break;
+    case 'EXCEPTION':
+    case 'REPLY':
+      let { resolve, reject } = that[STORAGE].take(id);
+      let field = fields[0];
+      if (field.id) {
+        let errorType = api.throws.find(item => +item.id === +field.id);
+        if (errorType) {
+          let type = errorType.name;
+          let data = that.schema.decodeValueWithType(field, errorType.type);
+          reject({ name: 'THRIFT_EXCEPTION', type, data });
+        } else {
+          reject({ name: 'THRIFT_ERROR', field });
+        }
+      } else {
+        try {
+          resolve(that.schema.decodeValueWithType(field, api.type));
+        } catch (reason) {
+          reject(reason);
+        }
+      }
+      break;
+    default:
+      throw Error('No Implement');
+  }
+}
+
+class ThriftClient extends EventEmitter {
   static start({ port, schema }) {
     return new ThriftListener({ port, schema });
   }
   constructor(options) {
+    super();
     Object.keys(options).forEach(key => this[key] = options[key]);
     if (!('retryDefer' in this)) this.retryDefer = 1000;
+    Object.defineProperty(this, METHODS, { value: {} });
+    Object.defineProperty(this, STORAGE, { value: new Storage() });
   }
   set schema(data) {
     let schema = new ThriftSchema(data);
@@ -56,8 +100,8 @@ class ThriftClient {
     Object.defineProperty(this, 'schema', desc);
   }
   set thrift(thrift) {
-    thrift.on('error', reason => this[ERROR](reason));
-    thrift.on('data', message => this[RECEIVE](message));
+    thrift.on('error', reason => tcError(this, reason));
+    thrift.on('data', message => tcReceive(this, message));
     let desc = Object.getOwnPropertyDescriptor(ThriftClient.prototype, 'thrift');
     desc.get = () => thrift;
     Object.defineProperty(this, 'thrift', desc);
@@ -89,74 +133,6 @@ class ThriftClient {
   trigger(name, ctx) {
     return Promise.resolve(ctx).then(this[METHODS][name]);
   }
-
-  /**
-   * Private
-   **/
-  get [METHODS]() {
-    let value = {};
-    Object.defineProperty(this, METHODS, { value });
-    return value;
-  }
-  get [STORAGE]() {
-    let value = new Storage();
-    Object.defineProperty(this, STORAGE, { value });
-    return value;
-  }
-  [RECEIVE](message) {
-    let { id, type, name, fields } = message;
-    let api = this.schema.service[name];
-    switch (type) {
-      case 'CALL':
-        let params = this.schema.decodeStruct(api.args, { fields });
-        this.trigger(name, params).then(result => {
-          result = this.schema.encodeValueWithType(result, api.type);
-          result.id = 0;
-          let fields = [ result ];
-          this.thrift.write({ id, type: 'REPLY', name, fields });
-        }, error => {
-          let fields;
-          try {
-            fields = this.schema.encodeStruct(api.throws, error).fields;
-            if (!fields.length) throw error;
-          } catch (error) {
-            let { name, message } = error;
-            fields = [ { id: 999, type: 'STRING', value: JSON.stringify({ name, message }) } ];
-          }
-          this.thrift.write({ id, type: 'REPLY', name, fields });
-        });
-        break;
-      case 'EXCEPTION':
-      case 'REPLY':
-        let { resolve, reject } = this[STORAGE].take(id);
-        let field = fields[0];
-        if (field.id) {
-          let errorType = api.throws.find(item => +item.id === +field.id);
-          if (errorType) {
-            let type = errorType.name;
-            let data = this.schema.decodeValueWithType(field, errorType.type);
-            reject({ name: 'THRIFT_EXCEPTION', type, data });
-          } else {
-            reject({ name: 'THRIFT_ERROR', field });
-          }
-        } else {
-          try {
-            resolve(this.schema.decodeValueWithType(field, api.type));
-          } catch (reason) {
-            reject(reason);
-          }
-        }
-        break;
-      default:
-        throw Error('No Implement');
-    }
-  }
-  [ERROR](reason) {
-    this[STORAGE].takeForEach(({ reject }) => reject(reason));
-    if (this.retryDefer > 0) setTimeout(() => this.resetThrift(), this.retryDefer);
-    if (typeof this.onError === 'function') this.onError();
-  }
-
 }
 
 module.exports = ThriftClient;
